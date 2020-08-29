8f666b60d40fb4d5ab1d7b03cec6165c213db2ab
Luca <lucaalkafiri@gmail.com>
2020-08-29 22:39:56 +0400

    location issue

--- helpers.py
+++ helpers.py
@@ -22,8 +22,18 @@ def login_required(f):
     return decorated_function
 
 def location():
-  IPAddr = socket.gethostbyname(urllib.request.urlopen("https://ip.42.pl/raw").read())
+    # IPAddr = socket.gethostbyname(urllib.request.urlopen("https://ip.42.pl/raw").read())
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    geo_lookup = GeoLookup(os.getenv("API_KEY"))
+    location = geo_lookup.get_location(IPAddr)
+    return (location["city"], location["country_name"])
 
-  geo_lookup = GeoLookup(os.getenv("API_KEY"))
-  location = geo_lookup.get_location(IPAddr)
-  return (location["city"], location["country_name"])
+
+# def ip():
+#     hostname = socket.gethostname()
+#     ## getting the IP address using socket.gethostbyname() method
+#     ip_address = socket.gethostbyname(hostname)
+#     ## printing the hostname and ip_address
+#     print(f"IP Address: {ip_address}")
+
