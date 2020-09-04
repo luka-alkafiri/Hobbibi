ed70780fe21f29d33e596b5c96009cd147856e19
Luca <lucaalkafiri@gmail.com>
2020-09-05 01:49:43 +0400

    cookies update

--- app.py
+++ app.py
@@ -20,17 +20,17 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # responses aren't cached
-# @app.after_request
-# def after_request(response):
-#     response.headers["Cache_control"] = "no-cache, no-store, must-revalidate"
-#     response.headers["Expires"] = 0
-#     response.headers["Pragma"] = "no-cache"
-#     return response
+@app.after_request
+def after_request(response):
+    response.headers["Cache_control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
